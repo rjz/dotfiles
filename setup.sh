317f921472277943a0d7f41e224f63dd64fff3c5
rjz <rj@rjzaworski.com>
2016-02-17 09:15:19 -0800

    More vim plugins

--- setup.sh
+++ setup.sh
@@ -29,12 +29,16 @@ function install_vim_plugins () {
     chase/vim-ansible-yaml \
     chrisbra/csv.vim \
     leafgarland/typescript-vim \
+    vim-airline/vim-airline \
+    airblade/vim-gitgutter \
   ; do
     target="$BUNDLE_PATH/$(basename $repo)"
     if [[ -d "$target" ]]; then
-      bash -c "cd $target && git fetch && git pull origin master && cd $DIR"
+      echo "'$repo' (vim plugin): Checking github for updates ..."
+      bash -c "cd $target && git fetch && git pull -q origin master && cd $DIR"
     else
-      git clone https://github.com/${repo}.git $target
+      echo "'$repo' (vim plugin): Installing from github..."
+      git clone https://github.com/${repo}.git $target > /dev/null
     fi
   done
 }
